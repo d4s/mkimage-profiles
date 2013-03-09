dcb7fdd1ba6a0acbb6c3d3274ca726e28f5a94c1
Denis Pynkin <dans@altlinux.org>
2013-03-09 19:58:53 +0300

    Config for images

--- conf.d/evm.mk
+++ conf.d/evm.mk
@@ -1,12 +1,15 @@
 # evm images
 ifeq (distro,$(IMAGE_CLASS))
-distro/evm-desktop: distro/.live-desktop-ru use/live/autologin use/x11/3d-proprietary use/evm/desktop
+distro/live-evm-desktop: distro/.live-desktop-ru use/live/autologin use/x11/3d-proprietary use/evm/desktop
 	@$(call try,HOMEPAGE,http://bsuir.by)
 	@$(call try,HOMENAME,BSUIR)
 
-distro/evm-cluster: distro/.live-desktop-ru use/x11/3d-proprietary use/evm/cluster
+distro/live-evm-cluster: distro/.live-base use/evm/cluster use/relname
+	@$(call set,KFLAVOURS,led-ws)
 	@$(call try,HOMEPAGE,http://bsuir.by)
 	@$(call try,HOMENAME,BSUIR)
-	@$(call set,KFLAVOURS,led-ws)
 	@$(call set,RELNAME,EVM-Cluster)
+	@$(call set,META_SYSTEM_ID,LINUX)
+	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
+	@$(call set,META_VOL_SET,BSUIR EVM)
 endif
