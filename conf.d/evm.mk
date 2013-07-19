a19b377456f50e291d3c85002440588f67ccd210
Denis Pynkin <dans@altlinux.org>
2013-07-19 12:32:26 +0300

    Fixed 3d and mkimage. Packages fixed

--- conf.d/evm.mk
+++ conf.d/evm.mk
@@ -9,13 +9,13 @@ distro/live-evm-cluster: distro/.live-base use/evm/cluster use/evm/devel use/rel
 	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
 	@$(call set,META_VOL_SET,BSUIR EVM)
 
-distro/live-evm-devel: distro/live-evm-cluster  use/evm/cluster use/evm/devel use/relname use/systemd
+distro/live-evm-devel: distro/live-evm-cluster  use/evm/cluster use/evm/devel use/relname use/init/systemd
 	@$(call set,KFLAVOURS,un-def)
 	@$(call set,META_SYSTEM_ID,LINUX)
 	@$(call set,RELNAME,EVM-Devel)
 
 #distro/live-evm-desktop: distro/live-evm-devel distro/.live-desktop-ru use/live/autologin use/x11/3d-proprietary use/evm/desktop
-distro/live-evm-desktop: distro/live-evm-devel distro/.live-desktop-ru use/live/nodm use/x11/3d-proprietary use/evm/desktop
+distro/live-evm-desktop: distro/live-evm-devel distro/.live-desktop-ru use/live/nodm use/x11/3d use/evm/desktop
 	@$(call set,KFLAVOURS,un-def)
 	@$(call try,HOMEPAGE,http://bsuir.by)
 	@$(call try,HOMENAME,BSUIR)
