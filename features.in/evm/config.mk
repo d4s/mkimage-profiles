d117a4760d9f4a6c980494d86916fb3a13556f36
Denis Pynkin <denis_pynkin@epam.com>
2015-05-11 15:16:33 +0300

    Working version with all needed tools

--- features.in/evm/config.mk
+++ features.in/evm/config.mk
@@ -1,4 +1,4 @@
-use/evm: use/live/hooks
+use/evm: use/live/hooks use/live/rw +efi
 	@$(call add_feature)
 	@$(call add,LIVE_PACKAGES,livecd-save-nfs)
 	@$(call add,LIVE_KMODULES,kvm)
@@ -9,14 +9,14 @@ use/evm: use/live/hooks
 
 use/evm/cluster: use/evm
 	@$(call add,LIVE_LISTS,evm-cluster)
-#	@$(call add,LIVE_LISTS,domain-client)
-	@$(call add,LIVE_LISTS,evm-cuda)
-	@$(call add,LIVE_LISTS,evm-calibre)
+#	@$(call add,LIVE_LISTS,evm-cuda)
+#	@$(call add,LIVE_LISTS,evm-calibre)
 
-use/evm/devel: use/evm
+use/evm/devel: use/evm use/dev/builder/full
 	@$(call add,LIVE_LISTS,evm-devel)
 
-use/evm/desktop: use/evm/cluster use/evm/devel distro/regular-kde4
+use/evm/desktop: distro/regular-kde4 +robotics use/live/ru
+#use/evm/desktop: use/evm/cluster use/evm/devel distro/regular-kde4 +robotics use/live/ru
 	@$(call add,LIVE_LISTS, $(call tags,(base || extra) && (archive || network)))
 	@$(call add,LIVE_LISTS,evm)
 	@$(call add,LIVE_LISTS,gns3)
