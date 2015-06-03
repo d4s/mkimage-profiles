c54647c46aeb2cfc45aaeb073f727538b8428069
Denis Pynkin <denis_pynkin@epam.com>
2015-06-03 09:56:29 +0300

    Version after review by mike@
    Feature evm is still exists.

--- conf.d/evm.mk
+++ conf.d/evm.mk
@@ -1,25 +1,18 @@
 # evm images
 ifeq (distro,$(IMAGE_CLASS))
 distro/live-evm-cluster: distro/.live-base use/evm/cluster use/evm/devel use/relname
-	@$(call set,KFLAVOURS,std-def)
-	@$(call try,HOMEPAGE,http://bsuir.by)
-	@$(call try,HOMENAME,BSUIR)
 	@$(call set,RELNAME,EVM-Cluster)
-	@$(call set,META_SYSTEM_ID,LINUX)
 	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
 	@$(call set,META_VOL_SET,BSUIR EVM)
+	@$(call try,HOMEPAGE,http://bsuir.by)
+	@$(call try,HOMENAME,BSUIR)
 	@$(call add,LIVE_LISTS,domain-client)
 
 distro/live-evm-devel: distro/live-evm-cluster  use/evm/cluster use/evm/devel  use/relname use/init/systemd
-	@$(call set,KFLAVOURS,std-def)
-	@$(call set,META_SYSTEM_ID,LINUX)
+	@$(call set,KFLAVOURS,un-def)
 	@$(call set,RELNAME,EVM-Devel)
 
-#distro/live-evm-desktop: distro/live-evm-devel distro/.live-desktop-ru use/live/autologin use/x11/3d-proprietary use/evm/desktop
-distro/live-evm-desktop: distro/live-evm-devel distro/.live-desktop-ru use/live/nodm use/x11/3d use/evm/desktop use/branding
-	@$(call set,KFLAVOURS,std-def)
+distro/live-evm-desktop: distro/regular-kde4 distro/live-evm-devel distro/.live-desktop-ru use/x11-autologin use/x11/3d use/evm/desktop use/branding
 #	@$(call set,BRANDING,altlinux-p7)
-	@$(call try,HOMEPAGE,http://bsuir.by)
-	@$(call try,HOMENAME,BSUIR)
 	@$(call set,RELNAME,EVM-Desktop)
 endif
--- features.in/evm/config.mk
+++ features.in/evm/config.mk
@@ -1,24 +1,40 @@
-use/evm: use/live/hooks use/live/rw +efi
+use/evm: use/live/hooks use/live/rw +efi \
+	 use/evm/services use/evm/control use/evm/deflogin
 	@$(call add_feature)
 	@$(call add,LIVE_PACKAGES,livecd-save-nfs)
-	@$(call add,LIVE_KMODULES,kvm)
-	@$(call add,CLEANUP_PACKAGES,'kernel-modules-drm-nouveau*')
-	@$(call set,META_SYSTEM_ID,LINUX)
+	@$(call add,THE_KMODULES,nvidia kvm)
 	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
 	@$(call set,META_VOL_SET,BSUIR EVM)
 
+use/evm/services: use/services
+	@$(call add,DEFAULT_SERVICES_ENABLE, rpcbind sshd kheaders)
+	@$(call add,DEFAULT_SERVICES_ENABLE, xinetd smartd bluetoothd)
+	@$(call add,DEFAULT_SERVICES_ENABLE, x11presetdrv NetworkManager)
+	@$(call add,DEFAULT_SERVICES_ENABLE, livecd-save-nfs alteratord)
+	@$(call add,DEFAULT_SERVICES_ENABLE, virtualbox libvirtd)
+
+	@$(call add,DEFAULT_SERVICES_DISABLE, bind cups livecd-online-repo)
+
+use/evm/control: use/control
+	@$(call add,CONTROL,wireshark-capture:public)
+	@$(call add,CONTROL,fusermount:public)
+	@$(call add,CONTROL,nfsmount:public)
+	@$(call add,CONTROL,ppp:public)
+
+use/evm/deflogin: use/deflogin
+	@$(call add,GROUPS,vboxusers vmusers netadmin _alteratord)
+
 use/evm/cluster: use/evm
-	@$(call add,LIVE_LISTS,evm-cluster)
-#	@$(call add,LIVE_LISTS,evm-cuda)
-#	@$(call add,LIVE_LISTS,evm-calibre)
+	@$(call add,LIVE_LISTS,evm/cluster)
+#	@$(call add,LIVE_LISTS,evm/cuda)
 
 use/evm/devel: use/evm use/dev/builder/full
-	@$(call add,LIVE_LISTS,evm-devel)
+	@$(call add,LIVE_LISTS,evm/devel)
 
-use/evm/desktop: distro/regular-kde4 +robotics use/live/ru use/live/install
+use/evm/desktop: use/evm +robotics use/live/ru use/live/install
 #use/evm/desktop: use/evm/cluster use/evm/devel distro/regular-kde4 +robotics use/live/ru
 	@$(call add,LIVE_LISTS, $(call tags,(base || extra) && (archive || network)))
-	@$(call add,LIVE_LISTS,evm)
-	@$(call add,LIVE_LISTS,gns3)
-	@$(call add,LIVE_KMODULES,virtualbox)
+	@$(call add,LIVE_LISTS,evm/desktop)
+#	@$(call add,LIVE_LISTS,gns3)
+	@$(call add,the_KMODULES,virtualbox)
 
