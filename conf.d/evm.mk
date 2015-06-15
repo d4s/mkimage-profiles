08ccdc932144d93c1e3f6f2e04b88fc49102d51a
Denis Pynkin <denis_pynkin@epam.com>
2015-06-15 15:50:31 +0300

    fixed EVM distro creation, added VM configuration

--- conf.d/evm.mk
+++ conf.d/evm.mk
@@ -1,6 +1,18 @@
 # evm images
 ifeq (distro,$(IMAGE_CLASS))
-distro/live-evm-cluster: distro/.live-base use/evm/cluster use/evm/devel use/relname
+
+distro/.evm_services: use/services
+	@$(call add,SERVICES_ENABLE,rpcbind sshd kheaders)
+	@$(call add,SERVICES_ENABLE,xinetd smartd bluetoothd)
+	@$(call add,SERVICES_ENABLE,x11presetdrv NetworkManager)
+	@$(call add,SERVICES_ENABLE,livecd-save-nfs alteratord)
+	@$(call add,SERVICES_ENABLE,virtualbox libvirtd)
+	@$(call add,SERVICES_DISABLE,bind cups)
+	@$(call add,SERVICES_DISABLE,systemd-journald)
+	@$(call add,SERVICES_DISABLE,livecd-online-repo livecd-tmpfs)
+
+distro/live-evm-cluster: distro/.live-base distro/.evm_services \
+			 use/evm/cluster use/evm/devel use/relname
 	@$(call set,RELNAME,EVM-Cluster)
 	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
 	@$(call set,META_VOL_SET,BSUIR EVM)
@@ -8,11 +20,19 @@ distro/live-evm-cluster: distro/.live-base use/evm/cluster use/evm/devel use/rel
 	@$(call try,HOMENAME,BSUIR)
 	@$(call add,LIVE_LISTS,domain-client)
 
-distro/live-evm-devel: distro/live-evm-cluster  use/evm/cluster use/evm/devel  use/relname use/init/systemd
+distro/live-evm-devel: distro/live-evm-cluster use/evm/devel \
+		       use/relname use/init/systemd
 	@$(call set,KFLAVOURS,un-def)
 	@$(call set,RELNAME,EVM-Devel)
 
-distro/live-evm-desktop: distro/regular-kde4 distro/live-evm-devel distro/.live-desktop-ru use/x11-autologin use/x11/3d use/evm/desktop use/branding
+distro/live-evm-desktop: distro/regular-kde4 distro/.live-desktop-ru  \
+			 distro/live-evm-devel use/evm/desktop \
+			 use/live/autologin use/branding \
+			 use/x11/3d use/x11/nvidia/optimus \
+			 use/x11/kde4/nm +nm \
+			 use/isohybrid use/syslinux/timeout/30
 #	@$(call set,BRANDING,altlinux-p7)
+	@$(call set,KFLAVOURS,std-def)
 	@$(call set,RELNAME,EVM-Desktop)
+
 endif
--- features.in/evm/config.mk
+++ features.in/evm/config.mk
@@ -1,19 +1,11 @@
 use/evm: use/live/hooks use/live/rw +efi \
-	 use/evm/services use/evm/control use/evm/deflogin
+	 use/evm/control use/evm/deflogin
 	@$(call add_feature)
 	@$(call add,LIVE_PACKAGES,livecd-save-nfs)
-	@$(call add,THE_KMODULES,nvidia kvm)
+	@$(call add,CLEANUP_PACKAGES,'kernel-modules-drm-nouveau*')
+	@$(call add,THE_KMODULES,nvidia kvm kvm-amd kvm-intel)
 	@$(call set,META_VOL_ID,BSUIR EVM $(IMAGE_NAME)/$(ARCH))
 	@$(call set,META_VOL_SET,BSUIR EVM)
-
-use/evm/services: use/services
-	@$(call add,DEFAULT_SERVICES_ENABLE, rpcbind sshd kheaders)
-	@$(call add,DEFAULT_SERVICES_ENABLE, xinetd smartd bluetoothd)
-	@$(call add,DEFAULT_SERVICES_ENABLE, x11presetdrv NetworkManager)
-	@$(call add,DEFAULT_SERVICES_ENABLE, livecd-save-nfs alteratord)
-	@$(call add,DEFAULT_SERVICES_ENABLE, virtualbox libvirtd)
-
-	@$(call add,DEFAULT_SERVICES_DISABLE, bind cups livecd-online-repo)
 
 use/evm/control: use/control
 	@$(call add,CONTROL,wireshark-capture:public)
@@ -28,13 +20,15 @@ use/evm/cluster: use/evm
 	@$(call add,LIVE_LISTS,evm/cluster)
 #	@$(call add,LIVE_LISTS,evm/cuda)
 
+
 use/evm/devel: use/evm use/dev/builder/full
 	@$(call add,LIVE_LISTS,evm/devel)
 
-use/evm/desktop: use/evm +robotics use/live/ru use/live/install
+use/evm/desktop: use/evm +robotics use/live/ru use/live/install use/live/desktop
 #use/evm/desktop: use/evm/cluster use/evm/devel distro/regular-kde4 +robotics use/live/ru
 	@$(call add,LIVE_LISTS, $(call tags,(base || extra) && (archive || network)))
 	@$(call add,LIVE_LISTS,evm/desktop)
+	@$(call add,LIVE_LISTS,evm/games)
 #	@$(call add,LIVE_LISTS,gns3)
 	@$(call add,the_KMODULES,virtualbox)
 
