65f12f718d411e4db947b26e6716be791b9bacca
Denis Pynkin <dans@altlinux.org>
2016-03-05 21:20:06 +0300

    Provided evm-virt image for live  virtualization

--- conf.d/evm.mk
+++ conf.d/evm.mk
@@ -7,6 +7,10 @@ distro/.evm_services: use/services
 	@$(call add,SERVICES_ENABLE,x11presetdrv NetworkManager)
 	@$(call add,SERVICES_ENABLE,livecd-save-nfs alteratord)
 	@$(call add,SERVICES_ENABLE,virtualbox libvirtd)
+	@$(call add,SERVICES_ENABLE,lxd lxc-net lxcfs cgmanager)
+	@$(call add,SERVICES_ENABLE,getty@tty1 getty@ttyS0)
+	@$(call add,SERVICES_ENABLE,livecd-net-eth)
+	@$(call add,SERVICES_DISABLE,livecd-online-repo livecd-tmpfs)
 	@$(call add,SERVICES_DISABLE,bind cups)
 	@$(call add,SERVICES_DISABLE,systemd-journald)
 	@$(call add,SERVICES_DISABLE,livecd-online-repo livecd-tmpfs)
@@ -20,16 +24,27 @@ distro/live-evm-cluster: distro/.live-base distro/.evm_services \
 	@$(call try,HOMENAME,BSUIR)
 	@$(call add,LIVE_LISTS,domain-client)
 
-distro/live-evm-devel: distro/live-evm-cluster use/evm/devel \
-		       use/relname use/init/systemd
+distro/live-evm-virt: distro/.base distro/.evm_services \
+		       use/power/acpi/button use/relname \
+		       use/evm/virt \
+		       use/tty/S0 \
+		       use/init/systemd/multiuser
 	@$(call set,KFLAVOURS,un-def)
+	@$(call set,RELNAME,EVM-Virtual)
+
+distro/live-evm-devel: distro/live-evm-virt \
+		       use/evm/devel
+#	@$(call set,KFLAVOURS,un-def)
+	@$(call set,KFLAVOURS,std-def)
 	@$(call set,RELNAME,EVM-Devel)
 
 distro/live-evm-desktop: distro/regular-kde4 distro/.live-desktop-ru  \
-			 distro/live-evm-devel use/evm/desktop \
+			 distro/live-evm-cluster \
+			 use/evm/devel use/evm/desktop \
 			 use/live/autologin use/branding \
 			 use/x11/3d use/x11/nvidia/optimus \
 			 use/x11/kde4/nm +nm \
+			 use/init/systemd \
 			 use/isohybrid use/syslinux/timeout/30
 #	@$(call set,BRANDING,altlinux-p7)
 	@$(call set,KFLAVOURS,std-def)
